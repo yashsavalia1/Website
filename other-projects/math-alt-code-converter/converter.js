471be17912e3686db73d4f2abbc77fd3fa25e1ef
yashsavalia1 <yashsavalia1@gmail.com>
2021-02-23 13:25:18 -0500

    fraction update

--- other-projects/math-alt-code-converter/converter.js
+++ other-projects/math-alt-code-converter/converter.js
@@ -12,7 +12,7 @@ submitButton.addEventListener("click", (e) => {
 function expression(value) {
     value = value.trim()
     if (value.charAt(0) == "\"" && value.charAt() == "\"") {
-        value = value.substring(1,value.length - 1)
+        value = value.substring(1, value.length - 1)
 
     } else {
         value = value.replaceAll("pi", "π");
@@ -37,6 +37,22 @@ function expression(value) {
         value = value.replaceAll("sum", "Σ")
         value = value.replaceAll("delta", "Δ")
         value = value.replaceAll("partd", "∂")
+        value = value.replaceAll("1/2", "&frac12;")
+        value = value.replaceAll("1/3", "&frac13;")
+        value = value.replaceAll("2/3", "&frac23;")
+        value = value.replaceAll("1/4", "&frac14;")
+        value = value.replaceAll("3/4", "&frac34;")
+        value = value.replaceAll("1/5", "&frac15;")
+        value = value.replaceAll("2/5", "&frac25;")
+        value = value.replaceAll("3/5", "&frac35;")
+        value = value.replaceAll("4/5", "&frac45;")
+        value = value.replaceAll("1/6", "&frac16;")
+        value = value.replaceAll("5/6", "&frac56;")
+        value = value.replaceAll("1/7", "&frac17;")
+        value = value.replaceAll("1/8", "&frac18;")
+        value = value.replaceAll("7/8", "&frac78;")
+        value = value.replaceAll("1/9", "&#8529;")
+        value = value.replaceAll("1/10", "&#8530;")
 
 
     }
