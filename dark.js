dd71c432f7bd1b27a92f0d64c64bcff6897c386c
yashsavalia1 <yashsavalia1@gmail.com>
2021-02-28 21:37:28 -0500

    dark mode

---
+++ dark.js
@@ -0,0 +1,19 @@
+function darkmode() {
+    if (document.cookie == "darkmode") {
+        document.getElementById("body").style.backgroundImage = "linear-gradient(#363636, rgb(27, 27, 27))"
+    } else if (document.cookie == "lightmode") {
+        document.getElementById("body").style.backgroundImage = "url(https://i.stack.imgur.com/spmUM.gif)"
+
+    }
+}
+
+function toggleDarkMode() {
+    if (document.cookie == "darkmode") {
+        document.cookie = "lightmode";
+        darkmode();
+    } else {
+        document.cookie = "darkmode";
+        darkmode();
+    }
+
+}
