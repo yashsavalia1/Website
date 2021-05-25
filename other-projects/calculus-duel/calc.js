823606be915e25e70ed242c8a42782fffc0003f7
yashsavalia1 <yashsavalia1@gmail.com>
2021-05-25 11:32:02 -0400

    calc duel fix

--- other-projects/calculus-duel/calc.js
+++ other-projects/calculus-duel/calc.js
@@ -2,8 +2,8 @@ function darkmode() {
     if (document.cookie == "darkmode") {
         document.getElementsByTagName("BODY")[0].style.backgroundImage = "linear-gradient(#363636, rgb(27, 27, 27))"
     } else {
-        document.getElementsByTagName("BODY")[0].style.background = "url(background.png) no-repeat"
-        document.getElementsByTagName("BODY")[0].style.backgroundSize = "1300pt 500pt"
+        document.getElementsByTagName("BODY")[0].style.background = "url(background.png)"
+        document.getElementsByTagName("BODY")[0].style.backgroundSize = "cover"
 
     }
 }
